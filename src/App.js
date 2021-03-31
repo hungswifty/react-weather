31b044dc7d6d894d76b1dfac03602e49c193c163
hungswifty <domanhhung4395@gmail.com>
2021-03-31 10:46:48 +0700

    Weather forecast first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
 
 export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+      console.log("aaa");
+      console.log(process);
+      console.log(process.env.REACT_APP_API_KEY);
+      await fetch(
+        `${
+          process.env.REACT_APP_API_URL
+        }/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${
+          process.env.REACT_APP_API_KEY
+        }`
+      )
+        .then(res => res.json())
+        .then(result => {
+          setData(result);
+          console.log(result);
+        });
+    };
+    fetchData();
+  }, [lat, long]);
+
+  return <div className="App" />;
 }
