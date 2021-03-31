4a1f05308ce3df75185a917d250b57192304d044
hungswifty <domanhhung4395@gmail.com>
2021-03-31 13:45:18 +0700

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from "react";
 import React, { useEffect, useState } from "react";
 import Weather from "./components/weather";
 import "./style.css";
@@ -10,19 +9,18 @@ export default function App() {
 
   const REACT_APP_API_KEY = "9f19142d24f7fb28a382af07a45407fd";
   const REACT_APP_API_URL = "https://api.openweathermap.org/data/2.5";
+
   useEffect(() => {
     const fetchData = async () => {
       navigator.geolocation.getCurrentPosition(function(position) {
         setLat(position.coords.latitude);
         setLong(position.coords.longitude);
       });
-
       await fetch(
         `${REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${REACT_APP_API_KEY}`
       )
         .then(res => res.json())
         .then(result => {
-          console.log("test123");
           setData(result);
           console.log(result);
         });
